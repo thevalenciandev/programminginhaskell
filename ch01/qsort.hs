7be6477099f02bd0cfa2ca9bcd6629f3c476e762
TVD <thevalenciandev@gmail.com>
2021-04-30 14:18:08 +0100

    Quicksort reverse fn

--- ch01/qsort.hs
+++ ch01/qsort.hs
@@ -3,3 +3,9 @@ qsort' (x:xs) = qsort' ys ++ [x] ++ qsort' zs
                 where
                   ys = [y | y<-xs, y<=x]
                   zs = [z | z<-xs, z>x]
+
+qsortrev [] = []
+qsortrev (x:xs) = qsortrev ys ++ [x] ++ qsortrev zs
+                  where
+                    ys = [y | y<-xs, y>=x]
+                    zs = [z | z<-xs, z<x]

bcc68d376300f258b94dceb9f236bdb033dc3ced
TVD <thevalenciandev@gmail.com>
2021-05-11 20:48:07 +0100

    Tic tac toe basic declarations and grid utilities

---
+++ ch11/TicTacToe.hs
@@ -0,0 +1,44 @@
+import Data.Char
+import Data.List
+import System.IO
+
+size :: Int
+size = 3
+
+type Grid = [[Player]]
+
+-- Ordering on constructors is determined by their position
+-- in the data declaration, ie. 0 < B < X
+-- B means Blank
+data Player = O | B | X
+              deriving (Eq, Ord, Show)
+
+next :: Player -> Player
+next O = X
+next B = B -- For completeness
+next X = O
+
+empty :: Grid
+empty = replicate size (replicate size B)
+
+full :: Grid -> Bool
+full g = (all (/=B) (concat g))
+
+-- turn empty = O. O will be the human player
+turn :: Grid -> Player
+turn g = if os <= xs then O else X
+         where
+            os = length (filter (==O) ps)
+            xs = length (filter (==X) ps)
+            ps = concat g
+
+wins :: Player -> Grid -> Bool
+wins p g = any line (rows ++ cols ++ dias)
+           where
+              line = all (== p)
+              rows = g
+              cols = transpose g
+              dias = [diag g, diag (map reverse g)]
+
+diag :: Grid -> [Player]
+diag g = [g !! n !! n | n <- [0..size-1]]

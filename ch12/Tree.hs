6577a6f990e2a6c64e13f2cbb6f154bbac3b7669
TVD <thevalenciandev@gmail.com>
2021-05-13 06:34:05 +0100

    Making a tree support the fmap fn

---
+++ ch12/Tree.hs
@@ -0,0 +1,7 @@
+data Tree a = Leaf a | Node (Tree a) (Tree a)
+              deriving Show
+
+instance Functor Tree where
+  -- fmap :: (a -> b) -> Tree a -> Tree b
+  fmap g (Leaf x)   = Leaf (g x)
+  fmap g (Node l r) = Node (fmap g l) (fmap g r)

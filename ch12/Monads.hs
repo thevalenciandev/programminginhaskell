47284e81e6771c7e7fdb491f92c6859904f33140
TVD <thevalenciandev@gmail.com>
2021-05-13 20:40:09 +0100

    Safeeval fn using Monads

--- ch12/Monads.hs
+++ ch12/Monads.hs
@@ -4,3 +4,15 @@ data Expr = Val Int | Div Expr Expr
 eval :: Expr -> Int
 eval (Val x)   = x
 eval (Div x y) = eval x `div` eval y
+
+safediv :: Int -> Int -> Maybe Int
+safediv _ 0 = Nothing
+safediv x y = Just (x `div` y)
+
+safeeval :: Expr -> Maybe Int
+safeeval (Val x)   = Just x
+safeeval (Div x y) = case safeeval x of
+                        Nothing -> Nothing
+                        Just n  -> case safeeval y of
+                                      Nothing -> Nothing
+                                      Just m  -> safediv n m

797ffa994be0761ca431faeaedb9641d45a1a032
TVD <thevalenciandev@gmail.com>
2021-05-16 07:39:56 +0100

    Exercise 1: Defining a Functor for binary tree with data in their nodes

--- ch12/Tree.hs
+++ ch12/Tree.hs
@@ -5,3 +5,11 @@ instance Functor Tree where
   -- fmap :: (a -> b) -> Tree a -> Tree b
   fmap g (Leaf x)   = Leaf (g x)
   fmap g (Node l r) = Node (fmap g l) (fmap g r)
+
+data Tree' a = Leaf' | Node' (Tree' a) a (Tree' a)
+             deriving Show
+
+instance Functor Tree' where
+  -- fmap :: (a -> b) -> Tree' a -> Tree' b
+  fmap g Leaf' = Leaf'
+  fmap g (Node' l x r) = Node' (fmap g l) (g x) (fmap g r)
